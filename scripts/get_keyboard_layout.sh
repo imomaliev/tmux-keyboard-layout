493817cf5eea605d0212c4e1239341c13cc961d5
hanool <29415530+hanool@users.noreply.github.com>
2022-09-22 22:04:05 +0900

    create special case for 'keyboardLayout Name' not existing in AppleSelectedInputSources

--- scripts/get_keyboard_layout.sh
+++ scripts/get_keyboard_layout.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-defaults read ~/Library/Preferences/com.apple.HIToolbox.plist AppleSelectedInputSources | grep "KeyboardLayout Name" | cut -f 2 -d "=" | tr -d ' ;."'
+{
+  # gets the keyboard layout name
+  defaults read ~/Library/Preferences/com.apple.HIToolbox.plist AppleSelectedInputSources | grep "KeyboardLayout Name" | cut -f 2 -d '=' | tr -d ' ;.\"'
+} || {
+  # get input mode if there is no keyboard layout name
+  defaults read ~/Library/Preferences/com.apple.HIToolbox.plist AppleSelectedInputSources | egrep -w 'Input Mode.*inputmethod' | cut -f 2 -d "=" | rev | cut -d'.' -f 1 | rev | tr -d ' ;."'
+}
